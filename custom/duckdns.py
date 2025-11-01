f1c25bd9a7ff4b071ac5850387b692225a004371
kraemr <93094049+kraemr@users.noreply.github.com>
2025-11-01 14:56:16 +0100

    fixed

--- custom/duckdns.py
+++ custom/duckdns.py
@@ -1,8 +1,7 @@
 import requests as req
 import os
 
-def update_ip_addresses(ipv4,ipv6,hostname):
-	token = os.getenv("DUCK_DNS_TOKEN")
+def update_ip_addresses(ipv4,ipv6,hostname,token):
 	url = f"https://www.duckdns.org/update?domains={hostname}&token={token}&ip={ipv4}&ipv6={ipv6}&verbose=true"
 	print(url)
 	res = req.get(url)
